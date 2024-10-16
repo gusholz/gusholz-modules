de4f85123e5e7c05a7c6ba0224896a6386267cad
Gustavo Holzmann <gusholz@Mac.irede.local>
2024-10-16 17:44:51 -0300

    fix: update Navbar componenet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/Navbar";
 import ThemeSelector from "@/components/themeSelector";
 import OverviewSection from "@/components/overviewSection";
 
